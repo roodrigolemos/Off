bb137981c38701b242a71a953f68f6ea3b067ce5
Rodrigo Lemos <joserodrigolemospinto@gmail.com>
2026-03-10 16:04:22 -0400

    initial project setup

---
+++ Off/OffApp.swift
@@ -0,0 +1,17 @@
+//
+//  OffApp.swift
+//  Off
+//
+//  Created by Rodrigo Lemos on 10/03/26.
+//
+
+import SwiftUI
+
+@main
+struct OffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
